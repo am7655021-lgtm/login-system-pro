12ade5ff7b5daaf3db10cdff9e94b37a5e89e6d1
Abdallh <am7655021@gmail.com>
2026-05-02 19:38:25 +0300

    Add test route to check if server is working

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/test', (req, res) => {
+    res.send('Hello World! Server is working.');
+});
+
 app.get('/login.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
