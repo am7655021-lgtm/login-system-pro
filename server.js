ff28a893bfdc2c8055b38c8baf167bca315e7be2
Abdallh <am7655021@gmail.com>
2026-05-02 19:40:37 +0300

    Add proper Content-Type headers for static files

--- server.js
+++ server.js
@@ -29,10 +29,12 @@ app.get('/admin.html', (req, res) => {
 
 // Serve static files explicitly
 app.get('/style.css', (req, res) => {
+    res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'style.css'));
 });
 
 app.get('/script.js', (req, res) => {
+    res.setHeader('Content-Type', 'application/javascript');
     res.sendFile(path.join(__dirname, 'script.js'));
 });
 
